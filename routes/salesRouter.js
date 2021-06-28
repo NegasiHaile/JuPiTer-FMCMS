fee6185c3860f683c4db3a2cfcbc0204e3270926
NEGASI <willkinghi@gmail.com>
2021-06-28 04:06:21 -0900

    sales module deleted

--- routes/salesRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const salesCntrl = require('../controllers/salesCntrl')
-const auth = require('../middleware/auth')
-
-router.get('', salesCntrl.salesDashboard)
-
-
-module.exports = router;
