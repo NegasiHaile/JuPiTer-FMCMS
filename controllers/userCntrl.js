5d92c0fb38efa2427aff5c8d1f9a4cb807f70101
NEGASI <willkinghi@gmail.com>
2021-07-07 22:19:08 -0900

    getUserDetail method added to fetch a detail of spesfic detail

--- controllers/userCntrl.js
+++ controllers/userCntrl.js
@@ -75,6 +75,17 @@ const userCntrl = {
         }
     },
 
+    getuserDetail: async(req, res) => {
+        try {
+            const user = await Users.findById(req.params.id).select('-password')
+            if (!user) return res.status(400).json({ msg: "User does not exist." })
+
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+
     getLogedInUser: async(req, res) => {
         try {
             const user = await Users.findById(req.user.id).select('-password')
